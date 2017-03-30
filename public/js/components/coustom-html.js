7ea884c45c0cc5223695e2bd0c867850ec74dff3
24wings <2121718893@qq.com>
2017-03-30 19:06:00 +0800

    重新创建文件

--- public/js/components/coustom-html.js
+++
@@ -1,7 +0,0 @@
-angular.module('myApp')
-    .component('customHtml', {
-        template: '<h1>hello world</h1>',
-        controller: function() {
-            this.customHtml = '<h1>hello world</h1>';
-        }
-    });
--- public/js/controllers/petLifeDetail.controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('myApp.controllers')
-    .controller('PetLifeDetailCtrl', function($http, $scope, $routeParams) {
-
-        $scope.petLife = { _id: $routeParams._id };
-        $http.get('data/getDataById?modelName=PetLife&_id=' + $routeParams._id).then(function(rtn) {
-            $scope.petLife = rtn.data;
-        });
-
-    });
